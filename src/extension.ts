3fb041d47a3f6f77bd64e97b702facabc7f05e39
Sergo <usakhelo@users.noreply.github.com>
2016-12-23 23:18:44 -0500

    test cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -16,41 +16,10 @@ class GoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 }
 
 export function activate(ctx: vscode.ExtensionContext): void {
-    // ...
     ctx.subscriptions.push(
         vscode.languages.registerDocumentSymbolProvider(
             'maxscript', new GoDocumentSymbolProvider()));
-    // ...
 }
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-//     // Use the console to output diagnostic information (console.log) and errors (console.error)
-//     // This line of code will only be executed once when your extension is activated
-//     console.log('Congratulations, your extension "vsc-maxscript" is now active!');
-
-//     // The command has been defined in the package.json file
-//     // Now provide the implementation of the command with  registerCommand
-//     // The commandId parameter must match the command field in package.json
-//     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-//         var editor = vscode.window.activeTextEditor;
-//         if (!editor) {
-//             return; // No open text editor
-//         }
-
-//         var selection = editor.selection;
-//         var text = editor.document.getText(selection);
-
-//         // Display a message box to the user
-//         vscode.window.showInformationMessage('Selected characters: ' + text.length);
-//         // Display a message box to the user
-//         vscode.window.showInformationMessage('Hello World!');
-//     });
-
-//     context.subscriptions.push(disposable);
-// }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
