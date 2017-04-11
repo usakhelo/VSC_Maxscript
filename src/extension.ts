9226c58541d157ef2122a03c4cac95bda2f10d97
Sergo <usakhelo@users.noreply.github.com>
2017-04-10 23:42:09 -0400

    started definition provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,24 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-class GoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+class MaxscriptDefinitionProvider implements vscode.DefinitionProvider {
+    public provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Definition> {
+        return new Promise<vscode.Definition>((resolve, reject) => {
+        let range = document.getWordRangeAtPosition(position);
+        let symbol = document.getText(range);
+        console.log(range);
+        console.log(symbol);
+        //probably should find first location of symbol in current file and in other file in workspace
+        //should skip maxscript keywords, comments, string literals
+        //     if (!wordRange || lineText.startsWith('//') || isPositionInString(document, position) || word.match(/^\d+.?\d+$/) || goKeywords.indexOf(word) > 0) {
+        // return Promise.resolve(null);
+        //workspace.findFiles(ui5tsglobal.core.CreateRelativePath(tag[1]) + controllerFileEx, undefined);
+        new vscode.Location(document.uri, new vscode.Position(0, 0));
+        }); 
+    }
+}
+
+class MaxscriptDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
     public provideDocumentSymbols(
         document: vscode.TextDocument, token: vscode.CancellationToken):
         vscode.SymbolInformation[] {
@@ -16,10 +31,10 @@ class GoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
             let sym_str:string;
             while ((regex_result = funcReStr.exec(script_text)) !== null) {
                 if (regex_result.length > 0) {
-                    console.log(regex_result);
+                    // console.log(regex_result);
                     if (regex_result[1].length > 0) {
                         sym_str = regex_result[1] + " " +  regex_result[2];
-                        let loc = new vscode.Location(new vscode.Uri(), document.positionAt(funcReStr.lastIndex - sym_str.length));
+                        let loc = new vscode.Location(document.uri, document.positionAt(funcReStr.lastIndex - sym_str.length));
                         let sym = new vscode.SymbolInformation(sym_str, vscode.SymbolKind.Function, '', loc);
                         result.push(sym)
                     }
@@ -29,7 +44,7 @@ class GoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
                 if (regex_result.length > 0) {
                     if (regex_result[1].length > 0) {
                         sym_str = regex_result[1] + " " +  regex_result[2];
-                        let loc = new vscode.Location(new vscode.Uri(), document.positionAt(structReStr.lastIndex - sym_str.length));
+                        let loc = new vscode.Location(document.uri, document.positionAt(structReStr.lastIndex - sym_str.length));
                         let sym = new vscode.SymbolInformation(sym_str, vscode.SymbolKind.Function, '', loc);
                         result.push(sym)
                     }
@@ -43,9 +58,11 @@ class GoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 export function activate(ctx: vscode.ExtensionContext): void {
     ctx.subscriptions.push(
         vscode.languages.registerDocumentSymbolProvider(
-            'maxscript', new GoDocumentSymbolProvider()));
+            'maxscript', new MaxscriptDocumentSymbolProvider()));
+    ctx.subscriptions.push(
+        vscode.languages.registerDefinitionProvider(
+            'maxscript', new MaxscriptDefinitionProvider()));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
